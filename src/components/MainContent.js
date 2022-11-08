fc8b7fbd3b7b12c1511ec5e88f5e1d48d460f9ee
rekredhead <rekredhead@gmail.com>
2022-11-08 15:45:16 +0530

    Completed React Meme Generator

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,35 +1,43 @@
-import React from "react";
-import memesData from "./memesData";
+import React, { useEffect } from "react";
 
 export default function MainContent() {
     const buttonCSS = "text-white bg-gradient-to-r from-purple-900 to-fuchsia-600 "+
         "py-3 rounded-md mb-4 hover:opacity-70 active:scale-90";
     const inputCSS = "p-2 border w-full border-gray-400 rounded-md";
-    const memeText = "absolute text-white font-bold text-5xl text-stroke-black ";
+    const memeText = "absolute text-white font-bold text-5xl text-stroke-black "+
+        "left-2 right-2 text-center ";
 
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
-        randomImage: ""
+        randomImage: "http://i.imgflip.com/1bij.jpg"
     });
-    const [allMemeImages] = React.useState(memesData);
+    const [allMemes, setAllMemes] = React.useState([]);
+
+    useEffect(() => {
+        fetch('https://api.imgflip.com/get_memes')
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes));
+    }, []);
 
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes;
-        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const randomNumber = Math.floor(Math.random() * allMemes.length);
         setMeme(prevMeme => {
             return {
                 ...prevMeme,
-                randomImage: memesArray[randomNumber].url
+                topText: "",
+                bottomText: "",
+                randomImage: allMemes[randomNumber].url
             };
         });
     }
     
     function handleChange(event) {
+        const {name, value} = event.target;
         setMeme(prevMeme => {
             return {
                 ...prevMeme,
-                [event.target.name]: event.target.value
+                [name]: value
             }
         });
     }
@@ -50,10 +58,10 @@ export default function MainContent() {
             </button>
             <div className="flex relative justify-center items-center">
                 <img className="w-auto h-full" src={meme.randomImage} alt=""/>
-                <h1 className={memeText + "top-5"}>
+                <h1 className={memeText + "top-2"}>
                     {meme.topText}
                 </h1>
-                <h1 className={memeText + "bottom-5"}>
+                <h1 className={memeText + "bottom-2"}>
                     {meme.bottomText}
                 </h1>
             </div>
